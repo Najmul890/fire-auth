645db22418c974bdb53f8a522e40a2fa53b88529
Najmul890 <najmul890890@gmail.com>
2020-03-27 11:40:25 +0600

    final auth

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+  apiKey: "AIzaSyA0Q8VoG8f0UpsFikm765_8U5K6Pb1OYDA",
+  authDomain: "ema-jhon-simple-890.firebaseapp.com",
+  databaseURL: "https://ema-jhon-simple-890.firebaseio.com",
+  projectId: "ema-jhon-simple-890",
+  storageBucket: "ema-jhon-simple-890.appspot.com",
+  messagingSenderId: "746800219861",
+  appId: "1:746800219861:web:0db4f11fd485721ab6045c"
 };
-  export default firebaseConfig;
+export default firebaseConfig;
